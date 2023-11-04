5d40eaebfe0f056d030d25d2ee5fd1523ce5151e
Laura Cabra <lh.cabra@uniandes.edu.co>
2023-11-04 16:24:59 -0500

    Parcial terminado según instrucciones

--- ViveroElOtono/src/app/app.component.spec.ts
+++ ViveroElOtono/src/app/app.component.spec.ts
@@ -1,11 +1,13 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { PlantaListComponent } from './planta/planta-list/planta-list.component';
 
 describe('AppComponent', () => {
   beforeEach(() => TestBed.configureTestingModule({
-    imports: [RouterTestingModule],
-    declarations: [AppComponent]
+    imports: [RouterTestingModule,HttpClientTestingModule],
+    declarations: [AppComponent, PlantaListComponent]
   }));
 
   it('should create the app', () => {
@@ -20,10 +22,5 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('ViveroElOtono');
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('ViveroElOtono app is running!');
-  });
+ 
 });
--- ViveroElOtono/src/app/planta/planta-list/planta-list.component.spec.ts
+++ ViveroElOtono/src/app/planta/planta-list/planta-list.component.spec.ts
@@ -2,16 +2,23 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
+import { faker } from '@faker-js/faker';
 
 import { PlantaListComponent } from './planta-list.component';
+import { HttpClientModule } from '@angular/common/http';
+import { PlantaService } from '../planta.service';
+import { Planta } from '../planta';
 
 describe('PlantaListComponent', () => {
   let component: PlantaListComponent;
   let fixture: ComponentFixture<PlantaListComponent>;
+  let debug: DebugElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ PlantaListComponent ]
+      imports: [HttpClientModule],
+      declarations: [ PlantaListComponent ],
+      providers: [PlantaService]
     })
     .compileComponents();
   }));
@@ -19,10 +26,31 @@ describe('PlantaListComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(PlantaListComponent);
     component = fixture.componentInstance;
+    for(let i = 0; i < 3; i++) {
+      const planta = new Planta(
+        faker.datatype.number(),
+        faker.lorem.sentence(),
+        faker.lorem.sentence(),
+        faker.lorem.sentence(),
+        faker.datatype.number(),
+        faker.lorem.sentence(),
+        faker.lorem.sentence()
+      );
+      component.plantas.push(planta);}
     fixture.detectChanges();
+    debug = fixture.debugElement;
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+  it('should have 3 <tr planta> elements', () => {
+    expect(debug.queryAll(By.css('tr.planta'))).toHaveSize(3)
+  });
+  it('should have <tr encabezado> element', () => {
+    expect(debug.queryAll(By.css('tr.encabezado'))).toHaveSize(1)
+  });
+  it('should have 12 <td> elements', () => {
+    expect(debug.queryAll(By.css('td'))).toHaveSize(12)
   });
 });
