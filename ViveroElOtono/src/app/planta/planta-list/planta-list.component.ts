433aa03b7d6dc4c28e3cadcafa5687a11b3b9988
Laura Cabra <lh.cabra@uniandes.edu.co>
2023-11-03 23:03:38 -0500

    Punto 2 sin diseño

--- ViveroElOtono/src/app/planta/planta-list/planta-list.component.ts
+++ ViveroElOtono/src/app/planta/planta-list/planta-list.component.ts
@@ -8,14 +8,25 @@ import { PlantaService } from '../planta.service';
 })
 export class PlantaListComponent implements OnInit {
   plantas: Array<Planta> = [];
+  numeroInterior: number=0;
+  numeroExterior: number=0;
+
   constructor(private plantaService: PlantaService) {}
 
   getPlantas(): void {
     this.plantaService.getPlantas().subscribe((plantas) => {
       this.plantas = plantas;
-    });
-  }
+    });     
+    }
+  
+getInteriores(){
+  return this.numeroInterior =this.plantas.filter(planta => planta.tipo == 'Interior').length;
 
+}
+getExteriores(){
+  return this.numeroInterior =this.plantas.filter(planta => planta.tipo == 'Exterior').length;
+
+}
   ngOnInit() {
     this.getPlantas();
   }
